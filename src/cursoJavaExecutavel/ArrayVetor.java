12d3f0437ada803b3f9de20d0573bdc48d2fa8aa
leonardorledesma <leo.ledesma@gmail.com>
2022-05-27 02:09:03 -0300

    alterações na classe ArrayVetor

--- src/cursoJavaExecutavel/ArrayVetor.java
+++ src/cursoJavaExecutavel/ArrayVetor.java
@@ -1,17 +1,25 @@
 package cursoJavaExecutavel;
 
+import javax.swing.*;
+
 public class ArrayVetor {
     public static void main(String[] args) {
         /* Array pode ser de todos os tipos de dado e objetos */
         // Array sempre deve ter a quantidade de posições definidas
 
         // atribuir valores as posições do array
-        double[] notas = new double[5];
-        notas[0] = 7.8;
-        notas[1] = 8.7;
-        notas[2] = 9.7;
-        notas[3] = 9.9;
 
-        System.out.println(notas.toString());
+        String posicoes = JOptionPane.showInputDialog("Quantas posições o array deve ter?");
+        double[] notas = new double[Integer.parseInt(posicoes)];
+
+        for (int pos = 0; pos <notas.length; pos++){
+            String valor = JOptionPane.showInputDialog("Qual o valor da posição " + pos + " :");
+            notas[pos] = Double.valueOf(valor);
+        }
+
+
+       for (int pos = 0; pos < notas.length; pos++){
+           System.out.println("Nota " + (pos + 1) + " é = " + notas[pos]);
+       }
     }
 }
