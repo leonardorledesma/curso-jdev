f199ac672f42929ea54955fa56dfb61ec4411775
leonardorledesma <leo.ledesma@gmail.com>
2022-06-03 03:08:44 -0300

    adicionando botões com JButton

--- src/thread/TelaTimeThread.java
+++ src/thread/TelaTimeThread.java
@@ -13,6 +13,9 @@ public class TelaTimeThread extends JDialog {
     private JLabel descricaoHora2 = new JLabel("Time Thread 2");
     private JTextField mostraTempo2 = new JTextField();
 
+    private JButton jButton = new JButton("Start");
+    private JButton jButton2 = new JButton("Stop");
+
 
 
 
@@ -26,12 +29,16 @@ public class TelaTimeThread extends JDialog {
         GridBagConstraints gridBagConstraints = new GridBagConstraints();// controlador de posicionamento de componentes
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.insets = new Insets(5, 5, 5,5);
+        gridBagConstraints.anchor = GridBagConstraints.WEST;
 
         descricaoHora.setPreferredSize(new Dimension(200,25));
         jPanel.add(descricaoHora, gridBagConstraints);
 
         mostraTempo.setPreferredSize(new Dimension(200,25));
         gridBagConstraints.gridy ++;
+        mostraTempo.setEditable(false);
         jPanel.add(mostraTempo, gridBagConstraints);
 
         descricaoHora2.setPreferredSize(new Dimension(200,25));
@@ -40,7 +47,23 @@ public class TelaTimeThread extends JDialog {
 
         mostraTempo2.setPreferredSize(new Dimension(200,25));
         gridBagConstraints.gridy ++;
+        mostraTempo2.setEditable(false);
         jPanel.add(mostraTempo2, gridBagConstraints);
+
+        //**********************************************
+
+        gridBagConstraints.gridwidth = 1;
+
+        jButton.setPreferredSize(new Dimension(92,25));
+        gridBagConstraints.gridy ++;
+        jPanel.add(jButton, gridBagConstraints);
+
+        jButton2.setPreferredSize(new Dimension(92,25));
+        //gridBagConstraints.gridy ++;
+        gridBagConstraints.gridx ++;
+        jPanel.add(jButton2, gridBagConstraints);
+
+
 
 
         add(jPanel, BorderLayout.WEST);
