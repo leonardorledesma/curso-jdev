0759450cbd1b647dc2f696ac4e0b425c90596220
leonardorledesma <leo.ledesma@gmail.com>
2022-06-15 02:28:02 -0300

    adicionado pacote de arquivos

---
+++ src/arquivos/Arquivos.java
@@ -0,0 +1,47 @@
+package arquivos;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arquivos {
+    public static void main(String[] args) throws IOException {
+
+
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.setNome("leonardo");
+        pessoa1.setEmail("leo.ledesma@gmail.com");
+        pessoa1.setIdade(39);
+
+        Pessoa pessoa2 = new Pessoa();
+        pessoa2.setNome("leonardo2");
+        pessoa2.setEmail("leo2.ledesma@gmail.com");
+        pessoa2.setIdade(39);
+
+        Pessoa pessoa3 = new Pessoa();
+        pessoa3.setNome("leonardo3");
+        pessoa3.setEmail("leo3.ledesma@gmail.com");
+        pessoa3.setIdade(41);
+
+        List<Pessoa> pessoas = new ArrayList<Pessoa>();
+        pessoas.add(pessoa1);
+        pessoas.add(pessoa2);
+        pessoas.add(pessoa3);
+
+        File arquivo = new File("D:\\JDev\\listas2\\src\\arquivos\\arquivo.txt\\");
+
+        if(!arquivo.exists()){
+            arquivo.createNewFile();
+        }
+        FileWriter escrever_no_arquivo = new FileWriter(arquivo);
+
+        for (Pessoa p :pessoas) {
+            escrever_no_arquivo.write(p.getNome() + "; " + p.getEmail() + "; " + p.getIdade() + "\n");
+
+        }
+        escrever_no_arquivo.flush();
+        escrever_no_arquivo.close();
+    }
+}
--- src/modulo_datas/DatasEmJava8.java
+++ src/modulo_datas/DatasEmJava8.java
@@ -1,6 +1,7 @@
 package modulo_datas;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class DatasEmJava8 {
     public static void main(String[] args) {
@@ -15,6 +16,12 @@ public class DatasEmJava8 {
         System.out.println("Menos 1 ano: "+(database = database.minusYears(1)));
         System.out.println("Menos 1 mês: "+(database = database.minusMonths(1)));
         System.out.println("Menos 1 dia: "+(database = database.minusDays(1)));
+
+        for (int mes = 1; mes <= 12; mes++){
+            database = database.plusMonths(1);
+            System.out.println("Data de vencimento: " + database.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) +
+                    " do mês " + mes);
+        }
 
     }
 }
